0ba36a78819a8bad7b2233f53d7ae03a9f117b48
Daan Krol <daankrol@icloud.com>
2020-12-22 18:22:44 +0100

    LR

---
+++ linear_regression.py
@@ -0,0 +1,98 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import KFold
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import copy
+from read_data import get_pitch_duration_pairs, get_voice
+mpl.use('TkAgg')
+
+
+def construct_windows(pd_pairs, window_size):
+    windows = []
+
+    for i in range(len(pd_pairs)):
+        if i+window_size + 1 > len(pd_pairs) - 1:  # out of bounds
+            break
+        window = []
+        for j in range(window_size + 1):
+            window.append(pd_pairs[i+j][0])
+            window.append(pd_pairs[i+j][1])
+        # add Y value
+        window.append(pd_pairs[i+window_size+1][0])  # [0] for pitch
+        windows.append(window)
+    return np.array(windows)
+
+
+def use_kfold(voice, voice_windows):
+    X = voice_windows[:, :-1]
+    y = voice_windows[:, -1]
+
+    kf = KFold(n_splits=5)
+    for train_index, test_index in kf.split(X):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+
+        window_model = LinearRegression()
+        window_model.fit(X_train, y_train)
+        y_predictions = window_model.predict(X_test)
+        print(len(X_train), len(y_predictions))
+        break
+    plt.clf()
+    plt.plot(voice, 'b-')
+    plt.plot(y_predictions, 'r--')
+    plt.savefig('Kfold_ln_window_predict.png')
+
+
+def use_manual_split(voice, windows):
+    X_train = windows[:, :-1]  # 4/5
+    X_test = windows[2200:, :-1]  # 1/5
+    y_train = windows[:, -1]
+    y_test = windows[2200:, -1]
+
+    window_model = LinearRegression()
+    window_model.fit(X_train, y_train)
+
+    y_predictions = []
+    windows = windows[:, :-1]  # remove all y values
+
+    predicted_windows = [X_train[-1]]
+    for i in range(1000):
+        last_window = predicted_windows[-1]
+        # window = [p1, d1, p2, d2]
+
+        # shift right * 2 => window = [p2,d2, y , _, _]
+        # check if y == p2, then y = p2, d_new = d2+1
+        # else y = p_new,  d_new = 1
+        # new_window = [p2,d2,p3,d3,y]
+        pitch = round(window_model.predict([last_window])[0])
+        y_predictions.append(pitch)
+        previous_pitch, previous_duration = last_window[-2], last_window[-1]
+
+        duration = 1
+        # FIXME pitch prediction is a floating point value so this is never equal.
+        if pitch == previous_pitch:
+            duration = previous_duration + 1
+
+        new_window = last_window[2:]  # shift right, removing first p,d pair
+        new_window = np.append(new_window, pitch)
+        new_window = np.append(new_window, duration)
+        predicted_windows = np.vstack([predicted_windows, new_window])
+
+    print(y_predictions[0:100])
+    plt.clf()
+    plt.plot(voice, 'b-')
+    plt.plot(range(len(voice), len(voice)+1000), y_predictions, 'r--')
+    plt.savefig('ownFold.png')
+
+
+voice = get_voice(voice_number=3)
+pd_pairs = get_pitch_duration_pairs(voice_number=3, method='shift')
+windows = construct_windows(pd_pairs, 16)
+
+use_manual_split(voice, windows)
+
+
+# 5 keys
+# key 1 = [1, 0,0,0,0]
+[0, 43, 44, 45]
--- test.py
+++ test.py
@@ -1,3 +1,14 @@
-from read_data import *
+# from read_data import *
 
-print(get_pairs(voice_number=1, method='cumulative'))
+# print(get_pairs(voice_number=1, method='cumulative'))
+from sklearn.linear_model import LinearRegression
+import numpy as np
+model = LinearRegression()
+
+x = [1, 2, 3, 4, 5, 6]
+y = [[1, 0], [1, 0], [1, 0], [0, 1], [0, 1], [0, 1]]
+
+
+model.fit(np.array(x), np.array(y))
+
+model.predict([7])
