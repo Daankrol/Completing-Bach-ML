fbb24a0d40f4f8d39732950a47438bd7f822596b
jeroenvanbrandenburg <jeroen.vb@live.nl>
2021-01-02 10:46:24 +0100

    merge, adding methods

---
+++ methods.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+class WindowMethod(Enum):
+    CUMULATIVE = 1
+    SHIFT = 2
+
+class ProbabilityMethods(Enum):
+    RANGE = 1
+    VALUES = 2
+
+class SelectionMethod(Enum):
+    TOP = 1
+    WEIGHTED = 2
+    HIGHEST = 3
+    RANDOM = 4
+    
