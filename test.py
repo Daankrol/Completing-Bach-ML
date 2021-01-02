cb000979e7372d2bed47b8bb9d064cebb51d07da
jeroenvanbrandenburg <jeroen.vb@live.nl>
2021-01-02 17:14:20 +0100

    fixed mapdataset shapes

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ from window_generator import WindowGenerator
 # TODO label_columns does not exist yet
 w2 = WindowGenerator(input_width=6, label_width=1, shift=1)
 print(w2)
+x = w2.make_dataset(w2.train_df)
+print('x:', x)
 
 example_window = tf.stack([np.array(w2.train_df[:w2.total_window_size]),
                            np.array(w2.train_df[100:100+w2.total_window_size]),
@@ -12,13 +14,13 @@ example_window = tf.stack([np.array(w2.train_df[:w2.total_window_size]),
 
 example_inputs, example_labels = w2.split_window(example_window)
 
-print(example_labels)
+# print(example_labels)
 
-print(example_inputs)
+# print(example_inputs)
 
-print('All shapes are: (batch, time, features)')
-print(f'Window shape: {example_window.shape}')
-print(f'Inputs shape: {example_inputs.shape}')
-print(f'labels shape: {example_labels.shape}')
+# print('All shapes are: (batch, time, features)')
+# print(f'Window shape: {example_window.shape}')
+# print(f'Inputs shape: {example_inputs.shape}')
+# print(f'labels shape: {example_labels.shape}')
 
 # w2.make_dataset()
