f3e03ef8c54733a3d625c682bc349fe3a3cebb77
Daan Krol <daankrol@icloud.com>
2021-02-02 11:32:37 +0100

    merge of chiels changes

--- window_generator.py
+++ window_generator.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from data_frame import generate_dataframe
 import numpy as np
-import read_data
+import process_data
 
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
