9b6e798c706c7f33feb85f321c5dcfc48683baf3
Chiel <chielwijs@live.nl>
2021-02-14 12:09:00 +0100

    file rename

--- test.py
+++
@@ -1,8 +0,0 @@
-# import tensorflow as tf
-import numpy as np
-from process_data import *
-from window_generator import WindowGenerator
-import tensorflow as tf
-
-for i in [0,1,2,3]:
-    print(min(get_voice(i)))
